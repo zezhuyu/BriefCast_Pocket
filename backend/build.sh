a50f585818230df3fdb3926d2673f18719ecbd7f
Zezhu Yu <42472731+zezhuyu@users.noreply.github.com>
2025-06-18 19:59:56 -0400

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 
-pyinstaller --onefile --add-data "db/resources:db/resources" --add-data "db/schema.ddl:db" --add-data "db/rss.csv:db" --add-data ".env:." --collect-data language_tags --collect-data espeakng_loader --collect-data pymilvus --collect-data milvus_lite --collect-data apify_fingerprint_datapoints --icon=app.ico --name "briefcast-engine" api.py
+pyinstaller --onefile --add-data "db/resources:db/resources" --add-data "db/schema.ddl:db" --add-data "db/rss.csv:db" --add-data ".env:." --collect-data language_tags --collect-data espeakng_loader --collect-data pymilvus --collect-data milvus_lite --collect-data apify_fingerprint_datapoints --icon=app.ico --name "api" api.py
 
